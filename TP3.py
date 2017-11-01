787e397cbb3ff86bf42effa56a078a452d4c810f
Arayve <alejandro150998@outlook.com>
2017-10-31 21:22:09 -0300

    Importe sys , nos permitira leer el archivo sceql

--- TP3.py
+++ TP3.py
@@ -1,3 +1,4 @@
-##ARCHIVO DEL TP
-##HOLA MUNDO
-##fdakak
+import sys #https://sites.google.com/site/cursodepython/modulo-sys
+#import argparse https://rctorr.wordpress.com/2014/01/16/procesando-parametros-en-la-linea-de-comando-en-python/ , lo vamos a usar despues para verificar
+
+nombre_de_programa=sys.argv
