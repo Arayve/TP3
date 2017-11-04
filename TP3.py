07e83aa868da0e5606adfadfe4dfc9163ffbf50a
Arayve <alejandro150998@outlook.com>
2017-11-04 13:30:47 -0300

    COrrigiendo el nombre

---
+++ TP3.py
@@ -0,0 +1,16 @@
+import sys #https://sites.google.com/site/cursodepython/modulo-sys
+#import argparse https://rctorr.wordpress.com/2014/01/16/procesando-parametros-en-la-linea-de-comando-en-python/ , lo vamos a usar despues para verificar
+
+nombre_de_programa=sys.argv
+
+
+from cola import Cola
+
+cadena = " ________________________________________________________________________*_____________________________*_______**___*!=____________________________________________*=------------*________*=--------*=___*=------*=--------*_*!==__________*"
+
+def sceql (cadena):
+    
+    cola = Cola()
+    
+    cola.encolar(0)
+    
