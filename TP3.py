242975b81a0df68a8136f4114a44c753c4b003f4
alexis2013 <alexisalbanese2017@gmail.com>
2017-11-04 10:55:18 -0300

    agregue la clase cola

--- TP3.py
+++ TP3.py
@@ -2,3 +2,33 @@ import sys #https://sites.google.com/site/cursodepython/modulo-sys
 #import argparse https://rctorr.wordpress.com/2014/01/16/procesando-parametros-en-la-linea-de-comando-en-python/ , lo vamos a usar despues para verificar
 
 nombre_de_programa=sys.argv
+
+class _Nodo:
+    
+    def __init__ (self, dato, prox= None):
+        
+        self.dato = dato
+        
+        self.prox = prox
+        
+class Cola:
+    
+    def __init__(self):
+        
+        self.primero = None
+        
+        self.ultimo = None
+        
+    def encolar (self, dato):
+        
+        nodo = _Nodo(dato)
+        
+        if not self.prim:
+            
+            self.prim = nodo
+            
+        else:
+            
+            self.ultimo.prox = nodo
+            
+        self.ultimo = nodo
