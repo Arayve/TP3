2ddbe07dac0f5e2d9acbc147cab99b1300327e27
alexis2013 <alexisalbanese2017@gmail.com>
2017-11-08 23:19:21 -0300

    che, fijate que aca hce algo nuevo, pero no me funcan los ejemplos, me tira que las barras no estan en cantidades iguales

--- Auxiliar.py
+++ Auxiliar.py
@@ -1,101 +1,97 @@
-class _Nodo:
-    
-    def __init__ (self, dato, prox= None):
-        
-        self.dato = dato
-        
-        self.prox = prox
-            
-#-----------------------------------------------------------------------------
-class Sceql:
+from cola import Cola
 
-    def __init__(self):
+
+def sceql(cadena):
+
+    camino = {}
+    
+    barras = []
+    
+    for i in range(len(cadena)):
         
-        self.prim = None
+        c = cadena[i]
         
-        self.ultimo = None
-        
-    def encolar (self, dato):
-        
-        nodo = _Nodo(dato)
-        
-        if not self.prim:
+        if c == "\\":
             
-            self.prim = nodo
+            barras.append(i)
+
+        elif c == "/":
+
+            ultima_barra = barras.pop()
             
+            camino[ultima_barra] = i
+            
+def recorrer(cadena):
+    
+    camino = sceql(cadena)
+    
+    cola = Cola()
+    
+    cola.encolar(0)
+    
+    i = 0
+        
+    while i < len(cadena):
+        
+        caracter = cadena[i]
+
+        if caracter == "!" : 
+
+            cola.encolar(0)
+            
+            i += 1
+            
+        elif caracter == "=":
+
+            dato = cola.desencolar()
+
+            cola.encolar(dato)
+        
+            i += 1
+            
+        elif caracter == "-":
+
+            a = cola.ver_tope()
+                
+            dato = (a - 1) % 128
+                
+            cola.cambiar_primero(dato)
+            
+            i += 1
+            
+        elif caracter == "_" :
+
+            a = cola.ver_tope()
+                
+            dato = (a - 1) % 128
+                
+            cola.cambiar_primero(dato)
+            
+            i += 1
+            
+        elif caracter == "*" :
+
+            dato = cola.desencolar()
+              
+            letra = chr(dato)  
+            
+            print(letra , end="")
+                
+            cola.encolar(dato)
+            
+            i += 1
+                
+        elif caracter == "/":
+
+            if not cola.ver_tope():
+                 
+                continue
+             
+        elif caracter == "\\":
+            
+            i += 1
         else:
             
-            self.ultimo.prox = nodo
-            
-        self.ultimo = nodo
-        
-    def desencolar (self):
-        
-        nodo = self.prim
-        
-        dato = nodo.dato
-        
-        self.prim = nodo.prox
-        
-        return dato
-    
-    def esta_vacia (self):
-        
-        return self.prim is None
-    
-    def ver_primero(self):
-        
-        if not self.prim:
-            
-            raise ValueError()
-        
-        return self.prim.dato
-
-    def ejecutar(self,cadena):
-        #Borre el encolar del nodo , no se por que encolas un cero al inciio
-        if self.esta_vacia():
-            
-            self.prim = _Nodo(0)
-
-        for i in range(len(cadena)):
-            
-            caracter = cadena[i]
-        
-            if caracter == "!" : 
-            
-                self.encolar(0)
-            
-            elif caracter == "=":
-            
-                byte = self.desencolar()#si desencola algo vacio deberia hacer saltar un error especifico 
-        
-                self.encolar(byte)
-            
-            elif caracter == "-":
-                
-                dato = (self.prim.dato - 1) % 128
-                
-                self.prim.dato = dato
-            
-            elif caracter == "_" :
-                
-                dato = (self.prim.dato + 1) % 128
-
-                self.prim.dato = dato
-            
-            elif caracter == "*":
-                
-                dato = self.desencolar()
-              
-                print(chr(dato), end="")
-                
-                self.encolar(dato)
-                
-            elif caracter == "\\":
-                
-                
-
-
-x=Sceql()
-
-cadena = " ________________________________________________________________________*_____________________________*_______**___*!=____________________________________________*=------------*________*=--------*=___*=------*=--------*_*!==__________*"
+            i += 1
+                 
+recorrer(cadena)
